fa5c9ee903bdfe6221ae56d8115a8c6ae531f294
STEPH GASPAR <sg171@nyu.edu>
2016-05-20 14:16:57 -0400

    Created base Implementation of SQLiteDatabase
    (cherry picked from commit 19a20cfc1ca509a6317c82d995c5768301b779ec)
    
    Conflicts:
    	UwizardWPF/Setup.cs
    	UwizardWPF/UwizardWPF.csproj

---
+++ UwizardWPF/Setup.cs
@@ -0,0 +1,50 @@
+﻿using GalaSoft.MvvmLight.Ioc;
+using Microsoft.Practices.ServiceLocation;
+using UwizardWPF.Entities;
+using UwizardWPF.Server;
+using UwizardWPF.ViewModel;
+
+namespace UwizardWPF
+{
+    public class Setup
+    {
+        public void Register()
+        {
+            var container = SimpleIoc.Default;
+            ServiceLocator.SetLocatorProvider(() => container);
+            DoRegistration(container);
+            RegisterSchema(container.GetInstance<ISQLiteDatabase>());
+        }
+
+        protected virtual void DoRegistration(SimpleIoc container)
+        {
+            RegisterModels(container);
+            RegisterViewModels(container);
+            RegisterServices(container);
+        }
+
+        private void RegisterServices(SimpleIoc container)
+        {
+            container.Register<ISQLiteDatabase, SQLiteDatabase>();
+        }
+
+        private void RegisterViewModels(SimpleIoc container)
+        {
+            container.Register<MainViewModel>();
+            container.Register<GameManagementViewModel>();
+        }
+
+        private void RegisterModels(SimpleIoc container)
+        {
+        }
+
+        protected void RegisterSchema(ISQLiteDatabase db)
+        {
+            db.DoSync(connection =>
+            {
+                connection.CreateTable<WiiUDisk>();
+
+            });
+        }
+    }
+}
